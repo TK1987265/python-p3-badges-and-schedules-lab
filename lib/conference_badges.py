c3bd7b59e7acfc945ed218b6bb05b91f333bd447
Mohammad Khan <qcc2016@yahoo.com>
2024-06-28 16:14:18 -0400

    completed functions

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for index, name in enumerate(speakers):
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {index + 1}!")
+    return room_assignments
 
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    for message in badge_messages:
+        print(message)
+    for assignment in room_assignments:
+        print(assignment)
+
+
+if __name__ == "__main__":
+    names = ["Arel", "Carol"]
+    printer(names)
